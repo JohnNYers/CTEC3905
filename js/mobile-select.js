d0340c1d02a7de7f0e70eacae1947e3232f572f7
Johannes Baureithel <johannes.baureithel@hs-furtwangen.de>
2017-11-25 03:50:56 +0100

    custom select

--- js/mobile-select.js
+++ js/mobile-select.js
@@ -1,4 +1,22 @@
-!function ()
-{
-  let li = 
+! function () {
+  let mval = document.getElementById("dd-value");
+
+  let dropdown = document.getElementById("dd").getElementsByTagName("ul")[0];
+  let li = dropdown.getElementsByTagName("li");
+  let selected = document.getElementsByClassName("dd-select")[0];
+  mval.addEventListener("click", function () {
+    dropdown.classList.toggle("active");
+    mval.classList.toggle("active");
+  });
+  for (let i in li) {
+    li[i].addEventListener("click", function () {
+      selected.classList.remove("dd-select");
+      li[i].classList.add("dd-select");
+      mval.innerHTML = li[i].innerHTML;
+      selected = li[i];
+      dropdown.classList.remove("active");
+      mval.classList.remove("active");
+    })
+  }
+
 }();
